64af639127745dd6efa6b69fb5d87bb4c3de269d
kimyh <ikimyh059554@gmail.com>
2025-03-03 14:02:30 +0900

    feat: login, signup page

---
+++ js/login_password_showing.js
@@ -0,0 +1,22 @@
+/**
+ *  0303 hyun
+ * @param {input태그의 id} inputId
+ * @param {i 태그 자신} iconElement
+ */
+function togglePasswordVisibility(inputId, iconElement) {
+  let passwordInput = document.getElementById(inputId);
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    iconElement.classList.remove("fa-eye"); // 눈 뜬 모양
+    iconElement.classList.add("fa-eye-slash"); // 슬래시 눈 모양
+  } else {
+    passwordInput.type = "password";
+    iconElement.classList.remove("fa-eye-slash"); // 슬래시 눈 모양
+    iconElement.classList.add("fa-eye"); // 눈 뜬 모양
+  }
+}
+
+const goHomepage = () => {
+  window.location.href = "/index.html";
+};
