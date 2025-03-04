5f53fdf07d5e32f84b90d2a311679d7efb5746c0
sumin <tnals2565@naver.com>
2025-03-04 16:01:58 +0900

    feat: 로그인, 회원가입 페이지 반응형 UI 개발 완료

--- js/script.js
+++ js/script.js
@@ -8,4 +8,19 @@ document.addEventListener('DOMContentLoaded', function () {
       document.querySelector('footer').innerHTML = data[1];
     })
     .catch((error) => console.error('Error fetching HTML:', error))
+   
+  if (document.querySelector('.login-page')) {
+    const toggleButtons = document.querySelectorAll('.toggle-password');
+    const passwordFields = document.querySelectorAll('input[type="password"]');
+
+    toggleButtons.forEach((button, index) => {
+      button.addEventListener('click', function() {
+        const type = passwordFields[index].getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordFields[index].setAttribute('type', type);
+
+        this.querySelector('img').src =
+          type === 'password' ? 'images/common/ic_eye_off.svg' : 'images/common/ic_eye_on.svg';
+      });
+    });
+  }
 });
